5a3a2b50b17ab145c14c5368d2e97a6250510810
drammock <dan.mccloy@gmail.com>
2013-04-09 12:49:06 -0700

    tweaked acknowledgments.

--- scripts/BatchResynthRunner.sh
+++ scripts/BatchResynthRunner.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python BatchResynth.py /home/dan/Documents/academics/research/dissertation/stimuli/manipObjList.tab /home/dan/Documents/academics/research/dissertation/stimuli/manipulationObjects /home/dan/Documents/academics/research/dissertation/stimuli/textgrids_intensity_renamed /home/dan/Documents/academics/research/dissertation/stimuli/resynthesizedFiles 1 1 
+python BatchResynth.py /home/dan/Documents/academics/research/dissertation/stimuli/resynthSpreadsheets/manipObjList.tab /home/dan/Documents/academics/research/dissertation/stimuli/manipulationObjects /home/dan/Documents/academics/research/dissertation/stimuli/textgrids_intensity_renamed /home/dan/Documents/academics/research/dissertation/stimuli/resynthesizedFiles 1 1 
